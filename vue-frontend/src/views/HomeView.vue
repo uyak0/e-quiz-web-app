1312b6b03f460828597f57a95cd6527dc8c1b8d8
uyako <milkycoffeethings@gmail.com>
2024-01-20 08:40:47 +0800

    +prog

--- vue-frontend/src/views/HomeView.vue
+++ vue-frontend/src/views/HomeView.vue
@@ -1,30 +1,28 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-import axios from 'axios'
-import { onMounted, reactive } from 'vue';
+// import TheWelcome from '../components/TheWelcome.vue'
+// import axios from 'axios'
+// import { onMounted, reactive } from 'vue';
 
-const state = reactive ({
-  students: [],
-})
+// const state = reactive ({
+//   students: [],
+// })
 
-async function getData() {
-  await axios.get('http://localhost:8000/api/students')
-  .then(response => {
-    console.log(response.data)
-    state.students = response.data
-  })
-  .catch(error => {
-    console.log(error)
-  });
-}
+// async function getData() {
+//   await axios.get('http://localhost:8000/api/students')
+//   .then(response => {
+//     console.log(response.data)
+//     state.students = response.data
+//   })
+//   .catch(error => {
+//     console.log(error)
+//   });
+// }
 
-onMounted(getData)
+// onMounted(getData)
 </script>
 
 <template>
-  <main>
-      <div>
-        this is home page
-      </div>
-  </main>
+  <div class="h-full absolute left-0 font-jetBrains text-9xl">
+    E-Quizz
+  </div>
 </template>
