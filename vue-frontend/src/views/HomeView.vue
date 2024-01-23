58ef2541007ceac08391adab6ad59cf3c9e3ded3
uyak0 <milkycoffeethings@gmail.com>
2024-01-23 23:04:39 +0800

    +home page Progress

--- vue-frontend/src/views/HomeView.vue
+++ vue-frontend/src/views/HomeView.vue
@@ -22,13 +22,18 @@
 </script>
 
 <template>
+  <div class="absolute top-0 z-0 w-screen h-screen bg-cover bg-[url('/src/assets/HomePageBG.png')] opacity-10"></div>
+
   <nav class="font-jetBrains top-0 left-0 absolute w-full">
     <RouterLink to="/login" class="float-right bg-red-500 text-white font-bold border-0 rounded-md text-2xl px-3 mx-2 my-2">Login</RouterLink>
     <RouterLink to="/signup" class="float-right text-white border-0 rounded-md text-xl px-3 mx-2 my-3">Sign Up</RouterLink>
     <RouterLink to="/about" class="float-right text-white border-0 rounded-md text-xl px-3 mx-2 my-3">About Us</RouterLink>
   </nav>
 
-  <div class="h-full absolute left-0 font-jetBrains text-9xl font-extrabold">
-    E-Quizz
+  <div class="flex flex-col justify-center h-screen px-6">
+    <div class="wrapper font-jetBrains font-bold">
+      <h1 class="text-9xl text-white">E-Quizz</h1><br>
+      <h3 class="text-3xl text-slate-400">Your personal hub of Learning.</h3>
+    </div>
   </div>
 </template>
