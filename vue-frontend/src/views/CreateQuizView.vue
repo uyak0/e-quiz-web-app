adeb5c34a653da7ed8247da2cd8b0c977cf2e079
uyak0 <milkycoffeethings@gmail.com>
2024-02-21 00:45:14 +0800

    updated tailwind, +add answer button in MCQ
    Updated tailwind version because i was messing with WebStorm
    Added "add answer/option" button in MCQuestion.vue

--- vue-frontend/src/views/CreateQuizView.vue
+++ vue-frontend/src/views/CreateQuizView.vue
@@ -10,39 +10,46 @@
 
   let selected = ref('mcq')   // setting mcq as the default
   const date = ref('')
+  const questionNo = ref(1)
 
   const questionTypeChange = (newValue) => {
     selected.value = newValue;
   };
+
+  const questionCounter = () => {
+    questionNo.value++
+  }
 </script>
 
 <template>
   <div class="bg-slate-700 h-screen w-3/4 mx-auto">
     <TitleDateInsert v-model="date"/>
 
-    <div class="rounded-md border-2 mx-4 my-4 py-4">
-      <h3>Question #{{ questionNo }}</h3>
+    <!-- Question Component -->
+    <div class="rounded-md border-2 mx-4 my-4 py-4 px-4 font-jetBrains">
+      <h1 class="text-2xl">Question #{{ questionNo }}</h1>
       <div class="mx-4">
         <SelectQType v-model="selected" @update:question-type="questionTypeChange"/>
       </div>
+
       <!-- Multi-choice Question -->
       <div v-if="selected==='mcq'">
         <MCQuestion />
       </div>
 
       <!-- Subjective Question -->
-      <div v-if="selected==='sub'">
+      <div v-else-if="selected==='sub'">
         <SubjectiveQuestion />
       </div>
 
       <!-- True/False Question -->
-      <div v-if="selected==='tfq'">
+      <div v-else-if="selected==='tfq'">
         <TFQuestion />
       </div>
-
-
     </div>
-
+    <div class="border-dotted border-2 mx-4 my-4 py-4 px-4 rounded-md text-6xl text-center">
+      <button @click="questionCounter">+</button>
+    </div>
     <SubmitCancel />
   </div>
 
