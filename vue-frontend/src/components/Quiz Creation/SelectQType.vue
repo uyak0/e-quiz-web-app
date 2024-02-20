19d7b5ae11d5ecc1a69651771c2f0946ecad485b
uyak0 <milkycoffeethings@gmail.com>
2024-02-20 18:05:42 +0800

    +wip

--- vue-frontend/src/components/Quiz Creation/SelectQType.vue
+++ vue-frontend/src/components/Quiz Creation/SelectQType.vue
@@ -1,11 +1,17 @@
 <script setup>
+  const props = defineProps({
+    selected: String
+  })
 
+  props.selected = {
+    
+  }
 </script>
 
 <template>
   <select class="float-right text-slate-600">
-    <option value="mcq">Multi-choice</option>
-    <option value="sub">Subjective</option>
-    <option value="tfq">True/False</option>
+    <option value="{{ props.selected }}">Multi-choice</option>
+    <option value="{{ props.selected }}">Subjective</option>
+    <option value="{{ props.selected }}">True/False</option>
   </select>
 </template>
--- vue-frontend/src/views/CreateQuizView.vue
+++ vue-frontend/src/views/CreateQuizView.vue
@@ -1,28 +1,26 @@
 <script setup>
   import '@vuepic/vue-datepicker/dist/main.css'
-  import { RouterLink, RouterView } from 'vue-router'
   import { ref } from 'vue'
   import TitleDateInsert from '@/components/Quiz Creation/TitleDateInsert.vue'
   import SubmitCancel from '@/components/Quiz Creation/SubmitCancel.vue'
   import MCQuestion from '@/components/Quiz Creation/MCQuestion.vue'
   import SelectQType from '@/components/Quiz Creation/SelectQType.vue'
 
-  let selected = ref(null)
+  // let selected = ref(null)
   const date = ref('')
 </script>
 
 <template>
   <div class="bg-slate-700 h-screen w-3/4 mx-auto">
     <TitleDateInsert :date="date"/>
-    <RouterView />
     <SubmitCancel />
         
-    <div v-if="selected==='mcq'" class="rounded-md border-2 mx-4 my-4">
-      <MCQuestion />
+    <div class="rounded-md border-2 mx-4 my-4">
+      <div v-if="selected==='mcq'">
+        <MCQuestion />
+      </div>
+        <SelectQType :selected="selected"/>
     </div>
-
-    <!-- Question Type Selection -->
-    <SelectQType v-model="selected"/>
   </div>
 
 </template>
