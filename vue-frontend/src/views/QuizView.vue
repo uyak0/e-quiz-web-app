b9cbca9859eb9f20d8a7ba9a92573c5d8d46db0a
uyak0 <milkycoffeethings@gmail.com>
2024-03-04 14:51:17 +0800

    quiz view

--- vue-frontend/src/views/QuizView.vue
+++ vue-frontend/src/views/QuizView.vue
@@ -1,3 +1,41 @@
+<script setup>
+  const API = import.meta.env.VITE_LARAVEL_API;
+
+  let quizProps = ref([
+  { questionType: }
+  ])
+
+  function getQuizzes() {
+    axios.get(API + '/api/quizzes')
+      .then(response = () => {
+        quizProps.value = response.data 
+      })
+      .catch(error = () => {
+        console.log(error) 
+      })
+  }
+
+  onMounted(() => {
+    getQuizzes
+  })
+</script>
+
 <template>
-  
+  <div v-for="(item, index) in quizProps" :key="index">
+    <!-- Question Component -->
+    <div class="">
+      <h1>Question {{ index + 1 }}</h1>
+      <div v-if="item.questionType === mcq">
+        mcquestion
+      </div>
+
+      <div v-if="item.questionType === sub">
+        subjective question
+      </div>
+
+      <div v-if="item.questionType === tfq">
+        true/false
+      </div>
+    </div>
+  </div>
 </template>
