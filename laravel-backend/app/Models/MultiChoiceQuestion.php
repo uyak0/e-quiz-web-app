717b6b10ec6d4214832bf9136562e73691baab47
uyak0 <milkycoffeethings@gmail.com>
2024-03-13 16:33:00 +0800

    +QuizView

--- laravel-backend/app/Models/MultiChoiceQuestion.php
+++ laravel-backend/app/Models/MultiChoiceQuestion.php
@@ -4,14 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class MultiChoiceQuestion extends Model
 {
     use HasFactory;
     protected $table = 'multi_choice_questions';
     protected $fillable = ['description', 'options', 'question_no', 'correct_answer'];
+    protected $casts = ['options' => 'array'];
 
-    public function quiz()
+    public function quiz(): BelongsTo
     {
         return $this->belongsTo(Quiz::class);
     }
